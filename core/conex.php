bff874688c342ade83d8a0f4ef56b8cedf65878b
renereshtml <lucasreneres1010@gmail.com>
2025-09-03 11:35:16 -0300

    difici

---
+++ core/conex.php
@@ -0,0 +1,25 @@
+<?php
+function conecta() : mysqli
+{
+    $servidor = 'localhost';
+    $banco = 'blog01';
+    $port = 3306;
+    $usuario = 'root';
+    $senha = '';
+    $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $port);
+
+    if(!$conexao){
+        echo 'Erro: Não foi possível conectar ao MySQL.' . PHP_EOL;
+        echo 'Debugging errno: ' . mysqli_connect_errno() . PHP_EOL;
+        echo 'Debugging error: ' . mysqli_connect_error() . PHP_EOL;
+        return null;
+    }
+    return $conexao;
+}
+
+function desconecta($conexao)
+{
+    mysqli_close($conexao);
+}
+
+?>
---
+++ tests/sql_test.php
@@ -0,0 +1,37 @@
+<?php
+    require_once '../core/sql.php';
+
+    $id = 1;
+    $nome = 'lucas';
+    $email = 'luca@gmail.com';
+    $senha = '334200$';
+    $dados = ['nome' => $nome,
+              'email' => $email,
+              'senha' => $senha];
+
+    $entidade = 'usuario';
+    $criterio = [['id', '=', $id]];
+    $campos = ['id', 'nome', 'email'];
+    print_r($dados);
+    echo '<br>';
+    print_r($campos);
+    echo '<br>';
+    print_r($criterio);
+    echo '<br>';
+
+    // Teste geração INSERT
+    $instrucao = insert($entidade, $dados);
+    echo $instrucao.'<BR>';
+
+    // Teste geração UPDATE
+    $instrucao = update($entidade, $dados, $criterio);
+    echo $instrucao.'<BR>';
+
+    // Teste geração SELECT
+    $instrucao = select($entidade, $campos, $criterio);
+    echo $instrucao.'<BR>';
+
+    // Teste geração DELETE
+    $instrucao = delete($entidade, $criterio);
+    echo $instrucao.'<BR>';
+?>
